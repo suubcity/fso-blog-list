3a7a1eb0aed05b2d2f04a89b2290675bbc287e65
suubcity <suubcity@outlook.com>
2021-02-12 12:05:05 -0700

    app index logger and config re-factored

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const morgan = require('morgan');
+
+app.use(morgan('tiny'));
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+mongoose.connect(config.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+	Blog.find({}).then((blogs) => {
+		response.json(blogs);
+	});
+});
+
+app.post('/api/blogs', (request, response) => {
+	const blog = new Blog(request.body);
+
+	blog.save().then((result) => {
+		response.status(201).json(result);
+	});
+});
+
+module.exports = app;
