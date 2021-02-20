5a83fbce82c0f612cb8cfbc5c06793b72ea92713
suubcity <suubcity@outlook.com>
2021-02-20 11:59:25 -0700

    4.11*: Blog list tests, step4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -11,7 +11,7 @@ blogsRouter.post('/', (request, response) => {
 	const blog = new Blog(request.body);
 
 	blog.save().then((result) => {
-		response.status(201).json(result);
+		response.status(200).json(result);
 	});
 });
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -9,9 +9,11 @@ const api = supertest(app);
 
 beforeEach(async () => {
 	await Blog.deleteMany({});
+
 	const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
 	const promiseArray = blogObjects.map((blog) => blog.save());
-	Promise.all(promiseArray);
+	//if you forget await here the tests will run before DB is populated
+	await Promise.all(promiseArray);
 });
 
 test('get all blogs', async () => {
@@ -27,9 +29,45 @@ test('get all blogs', async () => {
 test('the unique identifier property of the blog posts is named id?', async () => {
 	const response = await api.get('/api/blogs');
 
+	//logging response.body
+	console.log(
+		'######',
+		'VARIABLE NAME:',
+		'response.body',
+		'TYPEOF:',
+		typeof response.body,
+		'VALUE:',
+		response.body,
+		'######'
+	);
+	//end of logging
+
 	response.body.forEach((blog) => {
 		expect(blog.id).toBeDefined();
 	});
+});
+
+test('post a new blog entry works', async () => {
+	const newBlog = {
+		title: 'Test Blog',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+	};
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(200)
+		.expect('Content-Type', /application\/json/);
+
+	const blogsAtEnd = await helper.blogsInDb();
+
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+	const titles = blogsAtEnd.map((blog) => blog.title);
+
+	expect(titles).toContain('Test Blog');
 });
 
 afterAll(() => {
